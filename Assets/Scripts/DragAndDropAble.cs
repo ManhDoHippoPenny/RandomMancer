f763728b8e1ddf3f37e3d599b3a38f0c3846b02a
ManhDoHippoPenny <manh@hippopenny.com>
2023-09-10 13:47:03 +0300

    initial commit

---
+++ Assets/Scripts/DragAndDropAble.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DefaultNamespace
+{
+    public class DragAndDropAble : MonoBehaviour
+    {
+        [SerializeField] private InputAction press, screen;
+
+        private Vector3 curScreenPos;
+
+        private Vector2 _dir;
+        private float _speed;
+
+        public Camera _camera;
+        private bool isDragging = false;
+        private PhysicBaseMovement _movement;
+        
+        private bool isClickedOn()
+        {
+            Ray ray = _camera.ScreenPointToRay(curScreenPos);
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(ray.origin, ray.direction);
+            Debug.Log(hit.collider);
+            return hit.transform == transform;
+        }
+
+        private Vector3 WorldPos
+        {
+            get
+            {
+                float z = _camera.WorldToScreenPoint(transform.position).z;
+                return _camera.ScreenToWorldPoint(curScreenPos + new Vector3(0,0,z));
+            }
+        }
+        
+        private void Awake()
+        {
+            _camera = Camera.main;
+            press.Enable();
+            screen.Enable();
+            _movement = GetComponent<PhysicBaseMovement>();
+            screen.performed += context =>
+            {
+                curScreenPos = context.ReadValue<Vector2>();
+            };
+            press.performed += _ =>
+            {
+                if(isClickedOn()) StartCoroutine(Drag());
+            };
+            press.canceled += _ =>
+            {
+                if (isDragging == true)
+                {
+                    isDragging = false;
+                    _movement.ApplyForce(_dir,_speed);
+                }
+            };
+        }
+        
+        
+        private IEnumerator Drag()
+        {
+            isDragging = true;
+            
+            //drag
+            Vector3 offset = transform.position - WorldPos;
+            //GetComponent<Rigidbody>().useGravity = false;
+            while (isDragging)
+            {
+                Vector3 tempPos = WorldPos + offset;
+                // if (tempPos.x < 0 || tempPos.y < 0 || tempPos.x > _camera.rect.width || tempPos.y > _camera.rect.height)
+                // {
+                //     isDragging = false;
+                //     _movement.ApplyForce(_dir,_speed);
+                //     StopCoroutine(Drag());
+                // }
+                _dir = - transform.position + tempPos;
+                _speed = Mathf.Max(Vector2.Distance(transform.position, tempPos),15f);
+                transform.position = tempPos;
+                yield return null;
+            }
+            
+            //drop
+            //GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PhysicBaseMovement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PhysicBaseMovement : MonoBehaviour
+    {
+        private Rigidbody2D _body;
+        [SerializeField] private float multiply;
+        [SerializeField] private float _speed;
+        [SerializeField] private Vector2 _movementDirection;
+        [SerializeField] private Vector2 speedBody;
+        
+        private void Awake()
+        {
+            _body = GetComponent<Rigidbody2D>();
+        }
+
+        public void ApplyForce(Vector2 dir, float speed)
+        {
+            this._speed = speed * multiply;
+            _body.velocity = dir * _speed;
+        }
+
+        private void Update()
+        {
+            speedBody = _body.velocity;
+        }
+
+        // private void FixedUpdate()
+        // {
+        //     _body.velocity = _movementDirection * _speed;
+        // }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.collider.gameObject.CompareTag("Wall"))
+            {
+                Debug.Log(col.contacts[0].normal);
+                _movementDirection = Vector2.Reflect(_movementDirection, col.contacts[0].normal);
+            } 
+        }
+    }
+}
