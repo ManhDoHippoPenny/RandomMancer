8c91e8ce8cbc96f37991df910ff88dcec8acea8d
manh@hippopenny.com <manh@hippopenny.com>
2023-09-20 12:16:22 +0300

    add assemblies

--- Assets/Scripts/Common/TowerSpawner.cs
+++ Assets/Scripts/Common/TowerSpawner.cs
@@ -7,11 +7,5 @@ namespace DefaultNamespace
         [SerializeField] private GameObject _TowerPrefab;
         [SerializeField] private GameObject _holder;
         
-        // public void SpawnTower(Vector2 position, TowerInfor infor)
-        // {
-        //     GameObject tower = Instantiate(_TowerPrefab, position, Quaternion.identity);
-        //     tower.transform.SetParent(_holder.transform);
-        //     tower.GetComponent<TowerBehaviour>().SetTowerInfor(infor);
-        // }
     }
 }
--- Assets/Scripts/Dice/DiceSpawnTower.cs
+++ Assets/Scripts/Dice/DiceSpawnTower.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class DiceSpawnTower : MonoBehaviour
     {
         
+        public void OnEnable()
+        {
+            GetComponent<PhysicBaseMovement>()._velocityOnZero += SpawnTowerAtLocation;
+        }
+
+        public void OnDisable()
+        {
+            GetComponent<PhysicBaseMovement>()._velocityOnZero -= SpawnTowerAtLocation;
+        }
+
+        public void SpawnTowerAtLocation()
+        {
+            
+        }
     }
 }
